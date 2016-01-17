c2080fe7d976c8e96d2be980122cfee52a821f0c
sm2016 <suzanab83@yahoo.com>
2016-01-17 18:47:55 +0100

    First commit

---
+++ clojure_pictures/test/clojure_pictures/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-pictures.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-pictures.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
