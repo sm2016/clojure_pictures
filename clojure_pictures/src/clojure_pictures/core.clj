0ee0de407765e68f45028d82d92a82017fd1fcd3
Suzana Miljkovic <suzanam@live.com>
2016-02-20 17:21:38 +0100

    Decoding is complete
    Finished the coding and decoding message into picture

--- clojure_pictures/src/clojure_pictures/core.clj
+++ clojure_pictures/src/clojure_pictures/core.clj
@@ -2,23 +2,23 @@
   (:gen-class))
 
 (use 'mikera.image.core)
-(use '[clojure.string :only (split)])
+(require '[mikera.image.filters :as filt])
 
-(def crypto-maps {})
-(def buffer-picture-first)
+(defn to-int [x]
+ (java.lang.Integer/parseInt x))
+
 
 (def file-with-cryptocode 
+  "takes a pointer to a file"
   (clojure.java.io/reader (clojure.java.io/file "cryptocode.txt")))
 
-(defn read-file 
+(defn read-file
+  "read the content of file"
   [file-with-cryptocode]
   (line-seq file-with-cryptocode))
 
 (def read-raw-cryptocode 
   (read-file file-with-cryptocode))
-
-(defn to-int [x]
- (java.lang.Integer/parseInt x)) 
 
 (defn read-crypto-data [contents-file]
   (reduce (fn [acc row]
@@ -28,7 +28,7 @@
                       :name crypto-mark})))
           {} contents-file))
 
-
+; u crypto-data mi se nalazi mapa sa crypto kodom
 (def crypto-data (read-crypto-data read-raw-cryptocode))
 
 (def crypto-maps {})
@@ -50,12 +50,34 @@
   (loop [index-sign 0]
     (when (< index-sign (count secret-message-x))
       (loop [index-crypto-data 10]
-        (when (< index-crypto-data (+ (count crypto-data) 10))
-          (if (= (str (get secret-message-x index-sign))(get (get crypto-data index-crypto-data) :name))
+        (when (< index-crypto-data (+ (count crypto-data) 20))
+          (if (= (str (get secret-message-x index-sign))(str (get (get crypto-data index-crypto-data) :name)))
             (def crypto-maps (assoc crypto-maps index-sign {:id (get (get crypto-data index-crypto-data) :id)})))
           (recur (+ index-crypto-data 1))))
       (recur (+ index-sign 1))))
   (insert-in-picture buffer-picture-first)) 
+
+(def crypto-maps-decoder {})
+(def index-decoder 0) 
+(def decrypt-message "")
+
+(defn decoding
+   "Deconding message"
+  []
+  (let [message ""]
+    (loop [index 1]
+      (when (< index (count crypto-maps-decoder))     
+      (loop [index-crypto-data 10]
+        (when (< index-crypto-data (+ (count crypto-data) 20))
+          (if (= (str (get (get crypto-maps-decoder index) :value) (get (get crypto-maps-decoder (+ index 1)) :value)) (str (get (get crypto-data index-crypto-data) :id)))
+                 (do 
+                   (def deo-message (str (get (get crypto-data index-crypto-data) :name)))
+                   (def decrypt-message (str decrypt-message deo-message))))
+          (recur (+ index-crypto-data 1))))     
+      (recur (+ index 2)))) 
+    (if (= decrypt-message "")
+      (println "")
+      (println (str "Your secret message is: " decrypt-message)))))
 
 (defn check-pixels
   "Verify that the pixels of the pictures are same"
@@ -64,8 +86,8 @@
       pixel2 (get-pixel picture-second width height)]
   (if (not (= pixel1 pixel2))
     (do 
-      (println (str "Coordinates: " width " " height " " pixel1 " " pixel2 " " (str (- pixel2 pixel1))))
-      (def messenger "abc")))))
+      (def same "samee")
+      (str (- pixel2 pixel1))))))
 
 (defn check-pixels-loop
   "prepare arguments for calling methods check-pixels and gives a final solution"
@@ -74,12 +96,19 @@
     (when (< width1 (width picture-first))
     (loop [height1 0]
       (when (< height1 (height picture-first))
-          (check-pixels picture-first picture-second width1 height1)
+      (let [res-check-pixels (check-pixels picture-first picture-second width1 height1)]
+        (if (= res-check-pixels nil)
+          (str "a")
+          (do 
+            (def index-decoder (+ index-decoder 1))
+            (def crypto-maps-decoder (assoc crypto-maps-decoder index-decoder {:value (to-int res-check-pixels)})))))
         (recur (+ height1 1))))
       (recur (+ width1 1))))
-  (if (string? messenger)
-    (println "The pictures are different!")
-    (println "The pictures are the same!")))
+  (if (string? same)
+    (println "The pictures are different! May contain a hidden message!")
+    (println "The images are the same! There is no hidden message!"))
+  (decoding))
+
 
 (defn check-height
   "Verify that the height of the pictures are same"
@@ -89,8 +118,7 @@
       (println (str "Height is same: " (height picture-first)))
       (check-pixels-loop picture-first picture-second))
     (do
-      (println (str "Height is differently: " " H1 = " (height picture-first) " H2 = " (height picture-second)))
-      (println (str "The pictures are different!")))))
+      (println (str "Height is differently: " " H1 = " (height picture-first) " H2 = " (height picture-second))))))
 
 (defn check-width
   "Verify that the width of the pictures are same"
@@ -98,10 +126,8 @@
   (if (= (width picture-first)(width picture-second))
     (do
       (println (str "Width is same: " (width picture-first)))
-      (check-height picture-first picture-second))
-    (do
-      (println (str "Width is differently: " " W1 = " (width picture-first) " W2 = " (width picture-second)))
-      (println (str "The pictures are different!")))))
+      (check-height picture-first picture-second))  
+    (println (str "Width is differently: " " W1 = " (width picture-first) " W2 = " (width picture-second)))))
 
 (defn get-input 
   "Take user's input and convert it to lowercase"
@@ -110,7 +136,16 @@
     (let [input (clojure.string/trim (read-line))] 
       (if (empty? input) 
         default 
-        (str input))))) 
+        (clojure.string/lower-case input))))) 
+
+(defn get-input-message 
+   "Waits for user to enter text and hit enter, then cleans the input" 
+   ([] (get-input-message "")) 
+   ([default] 
+     (let [input (str (read-line))] 
+       (if (empty? input) 
+         default 
+         (str input)))))
 
 (defn upload-pictures
   "Enter the path to first picture and enter the path to second/crypt picture"
@@ -130,9 +165,8 @@
   (let [path (String. (get-input 200))]
     (def buffer-picture-first (load-image path)))
   (println "Enter the message, save (File - Save as) and close the picture")
-  (let [message (String. (get-input 200))] 
-    (def secret-message message) 
-    (encoding-message secret-message)))
+  (let [message (String. (get-input-message 200))] 
+    (encoding-message message)))
 
 (defn selection-mod-work
   "Choose whether you want to encrypt or decrypt"
